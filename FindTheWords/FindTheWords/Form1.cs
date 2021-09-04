e6c554f21b85079dea8673f3c73894eb5313bc56
Mehmetali <72599968+bellurm@users.noreply.github.com>
2021-09-04 20:16:16 +0300

    If you have a word list and if you can't a specific word in your list use this program
    I was prepare a digital English-Turkish dictionary and I created this program to avoid typing a word twice. This program is useful.

---
+++ FindTheWords/FindTheWords/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FindTheWords
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.SelectionStart = textBox1.Text.Length;
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (listBox1.Items[i].ToString().Contains(textBox1.Text))
+                {
+                    listBox1.SetSelected(i, true);
+                    if (textBox1.Text == "")
+                    {
+                        listBox1.SetSelected(i, false);
+                    }
+                    else
+                    {
+                        listBox1.SetSelected(i, false);
+                    }
+                }
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.SelectionStart = textBox2.Text.Length;
+            for (int i = 0; i < listBox2.Items.Count; i++)
+            {
+                if (listBox2.Items[i].ToString().Contains(textBox2.Text))
+                {
+                    listBox2.SetSelected(i, true);
+                    if (textBox2.Text == "")
+                    {
+                        listBox2.SetSelected(i, false);
+                    }
+                    else
+                    {
+                        listBox2.SetSelected(i, false);
+                    }
+                }
+            }
+        }
+    }
+}
